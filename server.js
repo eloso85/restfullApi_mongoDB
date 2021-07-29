835c0f964e6079bc5103cc133a0a01da0f3f17c1
Alejandro <eloso85@gmail.com>
2021-07-29 14:11:45 -0500

    added gitignore

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const url = process.env.DATABASE_URL;
+const user = process.env.DB_USERNAME;
+const pass = process.env.DB_PASSWORD
+
+mongoose.connect(url, { 
+    useNewUrlParser:true,
+    dbName: "subscriber",
+    user: user,
+    pass: pass
+
+});
+const db = mongoose.connection
+db.on('error',(error) => console.error(error))
+
+app.listen(3000, ()=>console.log(' server started '))
