b6460b5fb342182fd91d4e07cbbb62470378dd66
Alejandro <eloso85@gmail.com>
2021-08-03 13:34:22 -0500

    added schema

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 
+//routes
+const subscriberRouter = require('./routes/subscribers');
+
+app.use('/subscribers', subscriberRouter)
+
 require('dotenv').config()
 
 const url = process.env.DATABASE_URL;
@@ -13,9 +18,12 @@ mongoose.connect(url, {
     dbName: "subscriber",
     user: user,
     pass: pass
-
 });
+
 const db = mongoose.connection
 db.on('error',(error) => console.error(error))
+db.once('open', ()=> console.log('connected to database'))
+
+app.use(express.json())
 
 app.listen(3000, ()=>console.log(' server started '))
